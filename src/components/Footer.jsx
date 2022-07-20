ecee7309781d5f35d7928799b98406264dfce652
César Freitas <cesarfreitascontato@gmail.com>
2022-07-20 14:10:25 -0300

    studying navigate

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <div className="container-fluid bg-dark text-white mt-5 ">
+                <div className="row">
+                    <div className="col text-center">
+                        <h3>Footer</h3>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
